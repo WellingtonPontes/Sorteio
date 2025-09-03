78fc30f495372114ec9331abbde91f120c753812
WELLINGTON FRANCISCO SOUZA PONTES <67726052+WellingtonPontes@users.noreply.github.com>
2025-09-03 19:42:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function sorteio(NAsortea,QuantidadeTotal) {
+    const NumerosSorteados = []
+
+    while (NumerosSorteados.length<NAsortea) {
+        let sortiado = Math.floor(Math.random()*QuantidadeTotal + 1)
+        if(!NumerosSorteados.includes(sortiado)){
+            NumerosSorteados.push(sortiado)
+        }
+    }
+
+    return MostraResultado(NumerosSorteados)
+}
+
+function MostraResultado(NumerosSorteados) {
+
+    const resultado = document.getElementById("resultado");
+    const jogo = document.getElementById("jogo")
+
+    if(NumerosSorteados.length == 5){
+        jogo.textContent = "Quina"
+    }else if(NumerosSorteados.length ==15){
+        jogo.textContent = "Lotomania"
+    }else if(NumerosSorteados.length == 6){
+        jogo.textContent = "Mega-sena"
+    }
+
+        const resultadoTexto = NumerosSorteados.join('-');
+        resultado.textContent = resultadoTexto;
+    
+}
